13c697f98a9088e3fa57c008ce28c3d4e87928c5
Krutik Aghera <agherakrutik99@gmail.com>
2023-04-06 09:47:13 +0530

    file metadata completed

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var cors = require('cors');
 require('dotenv').config()
+const multer = require('multer');
+const upload=multer({dest: 'uploads/'})
+
 
 var app = express();
 
@@ -10,6 +13,20 @@ app.use('/public', express.static(process.cwd() + '/public'));
 app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
+
+
+app.post('/api/fileanalyse' , upload.single('upfile') , (req, res)=>{
+
+  let resObj = {
+    name : req.file.originalname,
+    type : req.file.mimetype,
+    size : req.file.size
+  }
+
+  console.log(resObj);
+
+  res.json(resObj);
+})
 
 
 
